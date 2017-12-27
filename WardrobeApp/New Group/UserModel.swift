4daec6c891ee232eb259d3ec6fe67678f4674093
Igor Khomenko <rogahom@gmail.com>
2017-12-27 03:34:06 +0200

    add user model

---
+++ WardrobeApp/New Group/UserModel.swift
@@ -0,0 +1,18 @@
+//
+//  UserModel.swift
+//  WardrobeApp
+//
+//  Created by Igor Khomenko on 12/27/17.
+//  Copyright © 2017 Relorie. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class UserModel: Object {
+    @objc dynamic var userFirstName = ""
+    @objc dynamic var userLastName = ""
+    @objc dynamic var userNickName = ""
+    @objc dynamic var userGendr = ""
+    @objc dynamic var userAvatarData = Data()
+}
